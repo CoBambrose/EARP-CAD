c02e36f8b8f40eba45e97b72de430e1bdfc05696
CoBambrose <cobaniaca@gmail.com>
2020-09-06 10:33:47 +0100

    .

--- ic/dispatch/request.php
+++ ic/dispatch/request.php
@@ -12,7 +12,7 @@
   ];
 
   if (!isset($_SESSION['id']) || !isset($_GET['type'])) {
-    header('Location: ../');
+    // header('Location: ../');
   } else {
     $type = $_GET['type'];
     $unit = unitFromChar($conn, $_SESSION['c']);
@@ -49,6 +49,50 @@
       $stmt->bind_param("si", $types, $row['id']);
       $stmt->execute();
     }
-    header("Location: ../");
+    // header("Location: ../");
   }
 ?>
+
+<link rel="stylesheet" href="../../css/master.css">
+<style>
+  body {
+    display: grid;
+    min-height: 100vh;
+    overflow: hidden;
+    font-family: sans-serif;
+  }
+  div {
+    display: grid;
+    margin: auto;
+    padding: 2rem;
+    border-radius: .5rem;
+    background-color: #151515;
+    grid-template-rows: 1fr 1fr;
+    grid-row-gap: 2rem;
+  }
+  h2 {
+    margin: auto;
+    text-align: center;
+  }
+  form {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+  input {
+    padding: .3rem;
+    text-align: center;
+    color: #000;
+  }
+  input[type=submit] {
+    grid-column: 1/3;
+  }
+</style>
+<div>
+  <h2>What Location?</h2>
+  <form action="./request.php" method="get">
+    <input type="text" disabled value="<?php echo $typeEnumeration[$_GET['type']]; ?>">
+    <input type="hidden" name="type" value="<?php echo $_GET['type']; ?>">
+    <input type="text" autofocus name="location" placeholder="Location">
+    <input type="submit" value="Request">
+  </form>
+</div>
